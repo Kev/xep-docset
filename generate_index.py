8801df2335e983e2fd6b008523c951c9be405a78
Edwin Mons <edwin.mons@isode.com>
2016-11-05 13:04:05 +0100

    Directly connect to database from generate_index.py

--- generate_index.py
+++ generate_index.py
@@ -1,17 +1,19 @@
 import os
 import os.path
 import re
+import sqlite3
+import sys
 import xml.dom.minidom
 
-print 'CREATE TABLE IF NOT EXISTS searchIndex(id INTEGER PRIMARY KEY);'
-print 'DROP TABLE searchIndex;'
-print 'CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);'
-print 'CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);'
+if len(sys.argv) < 2:
+    print >>sys.stderr, "Usage: %s database" % (sys.argv[0],)
+    sys.exit(1)
 
-def putSQL(name, type, path):
-    print "INSERT OR IGNORE INTO searchIndex('name', 'type', 'path') VALUES ('%s', '%s', '%s');" % (name, type, path)
-
+verbose = True
+database_file = sys.argv[1]
+index = []
 xepre = re.compile('xep-0*([1-9][0-9]*).xml')
+
 for root, dirs, files in os.walk('xeps'):
     for fn in files:
         match = xepre.match(fn)
@@ -22,9 +24,9 @@ for root, dirs, files in os.walk('xeps'):
         path = 'xep-%04d.html' % (i,)
         type = 'Extension'
         name = "xep%d" % i
-        putSQL(name, type, path)
+        index += ((name, type, path),)
         name = "xep%04d" % i
-        putSQL(name, type, path)
+        index += ((name, type, path),)
         dom = xml.dom.minidom.parse(os.path.join(root, fn))
         header = dom.getElementsByTagName('header')
         title = header[0].getElementsByTagName('title')[0]
@@ -32,11 +34,23 @@ for root, dirs, files in os.walk('xeps'):
             if child.nodeType == child.TEXT_NODE:
                 title = child.data
         if title != "N/A":
-            putSQL(title, type, path)
+            index += ((title, type, path),)
         shortname = header[0].getElementsByTagName('shortname')
         if len(shortname) > 0:
             for child in shortname[0].childNodes:
                 if child.nodeType == child.TEXT_NODE:
                     shortname = child.data
             if shortname != 'NOT_YET_ASSIGNED':
-                putSQL(shortname, type, path)
+                index += ((shortname, type, path),)
+
+with sqlite3.connect(database_file) as conn:
+    cursor = conn.cursor()
+
+    cursor.execute('DROP TABLE IF EXISTS searchIndex;')
+    cursor.execute('CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);')
+    cursor.execute('CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);')
+
+    cursor.executemany("INSERT OR IGNORE INTO searchIndex('name', 'type', 'path') VALUES (?, ?, ?)", index)
+
+if verbose:
+    print "Generated %s for %d XEPs, %d items in total" % (database_file, len(set(item[2] for item in index)), len(index),)
