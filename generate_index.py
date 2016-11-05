d91f4e032b6755190a87144f1e464c9b7145c618
Edwin Mons <edwin.mons@isode.com>
2016-11-05 13:40:20 +0100

    Clean up generate_index.py
    Test-Information:
    Generated database looks sensible
    Found a XEP in the new docset

--- generate_index.py
+++ generate_index.py
@@ -5,6 +5,20 @@ import sqlite3
 import sys
 import xml.dom.minidom
 
+def getChildText(root, element_name, default=None, first_only=True):
+    result = []
+
+    for node in root.getElementsByTagName(element_name):
+        for child in node.childNodes:
+            if child.nodeType == child.TEXT_NODE:
+                result.append(child.data)
+
+        if first_only:
+            break
+
+    return ''.join(result) if len(result) else default
+
+
 if len(sys.argv) < 2:
     print >>sys.stderr, "Usage: %s database" % (sys.argv[0],)
     sys.exit(1)
@@ -15,40 +29,40 @@ index = []
 xepre = re.compile('xep-0*([1-9][0-9]*).xml')
 
 for root, dirs, files in os.walk('xeps'):
-    for fn in files:
-        match = xepre.match(fn)
+    for filename in files:
+        match = xepre.match(filename)
         if not match:
             continue
 
         i = int(match.group(1))
         path = 'xep-%04d.html' % (i,)
-        type = 'Extension'
-        name = "xep%d" % i
-        index += ((name, type, path),)
-        name = "xep%04d" % i
-        index += ((name, type, path),)
-        dom = xml.dom.minidom.parse(os.path.join(root, fn))
+        index_type = 'Extension'
+
+        index.append(("xep%d" % (i,), index_type, path))
+        index.append(("xep%04d" % (i,), index_type, path))
+
+        dom = xml.dom.minidom.parse(os.path.join(root, filename))
         header = dom.getElementsByTagName('header')
-        title = header[0].getElementsByTagName('title')[0]
-        for child in title.childNodes:
-            if child.nodeType == child.TEXT_NODE:
-                title = child.data
+
+        if len(header) == 0:
+            print >>sys.stderr, "Warning: %s does not have a header" % (filename,)
+            continue
+
+        title = getChildText(header[0], 'title', default='N/A')
         if title != "N/A":
-            index += ((title, type, path),)
-        shortname = header[0].getElementsByTagName('shortname')
-        if len(shortname) > 0:
-            for child in shortname[0].childNodes:
-                if child.nodeType == child.TEXT_NODE:
-                    shortname = child.data
-            if shortname != 'NOT_YET_ASSIGNED':
-                index += ((shortname, type, path),)
+            index.append((title, index_type, path))
+
+        shortname = getChildText(header[0], 'shortname', default='NOT_YET_ASSIGNED')
+        if shortname != 'NOT_YET_ASSIGNED':
+            index.append((shortname, index_type, path))
+
 
 with sqlite3.connect(database_file) as conn:
     cursor = conn.cursor()
 
-    cursor.execute('DROP TABLE IF EXISTS searchIndex;')
-    cursor.execute('CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);')
-    cursor.execute('CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);')
+    cursor.execute("DROP TABLE IF EXISTS searchIndex;")
+    cursor.execute("CREATE TABLE searchIndex('id' INTEGER PRIMARY KEY, 'name' TEXT, 'type' TEXT, 'path' TEXT);")
+    cursor.execute("CREATE UNIQUE INDEX anchor ON searchIndex ('name', 'type', 'path');")
 
     cursor.executemany("INSERT OR IGNORE INTO searchIndex('name', 'type', 'path') VALUES (?, ?, ?)", index)
 
